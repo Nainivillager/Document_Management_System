583bb25491623e2300696ecd347ffaadb692e165
HIMANSHU BHATT <himanshubhat358@gmail.com>
2024-11-30 12:44:58 +0530

    improved ui design

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="container mx-64 p-4">
+      <div className="overflow-x-auto">Dashboard admin</div>
+    </div>
+  );
+}
--- app/dashboard/super-admin/billing/page.tsx
+++ app/dashboard/super-admin/billing/page.tsx
@@ -25,9 +25,9 @@ const CompanyTable: React.FC = () => {
   };
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="container ml-60 mx-auto p-4">
       <div className="overflow-x-auto">
-        <table className="min-w-full bg-white shadow-md rounded-lg">
+        <table className="min-w-[80%] bg-white shadow-md rounded-lg">
           <thead className="bg-gray-50">
             <tr>
               <th className="py-2 px-4 border-b-2 border-gray-200 text-left text-sm font-semibold text-gray-600">Company ID</th>
