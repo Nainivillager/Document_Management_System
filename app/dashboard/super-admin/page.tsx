90efc150dff94ac8e9fba7ef018a8a128e5a8bc7
Adityaverma901 <verma.adii03@gmail.com>
2024-11-30 11:41:49 +0530

    added new branch

--- app/dashboard/super-admin/page.tsx
+++ app/dashboard/super-admin/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const page = () => {
+export default function page() {
   return (
-    <div>Admin Hu Maiiii........</div>
+    <div>admin hu mai</div>
   )
 }
 
-export default page;
+
