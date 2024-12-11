de6da1aa36d1577a0a7b306225d965b67b50e5f5
adityaverma901 <verma.adii03@gmail.com>
2024-12-11 16:54:53 +0530

    added company admin screen

---
+++ app/dashboard/company-admin/page.tsx
@@ -0,0 +1,11 @@
+import Compsider from '@/components/sider/company-sider'
+import React from 'react'
+
+export default function page() {
+  return (
+    <>
+    <div>page</div>
+    <Compsider />
+    </>
+  )
+}
